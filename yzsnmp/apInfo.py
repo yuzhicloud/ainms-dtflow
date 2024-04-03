b2be69acacfb24b68e2b145eb5452a12bbc24a4e
billyhotjava <billy.xiezhimin@gmail.com>
2024-04-03 14:43:58 +0800

    update

--- yzsnmp/apInfo.py
+++ yzsnmp/apInfo.py
@@ -15,26 +15,21 @@ def get_snmp_data():
     authProtocol = usmHMAC192SHA256AuthProtocol
     privProtocol = usmAesCfb256Protocol
 
-    # 感兴趣的OIDs
-
     oids = [
-        '1.3.6.1.4.1.2011.6.139.13.3.3.1'
+        '.1.3.6.1.4.1.2011.6.139.13.3.3.1.1',
+        '.1.3.6.1.4.1.2011.6.139.13.3.3.1.2',
+        '.1.3.6.1.4.1.2011.6.139.13.3.3.1.3',
+        '1.3.6.1.4.1.2011.6.139.13.3.3.1.4',
+        '1.3.6.1.4.1.2011.6.139.13.3.3.1.5',
+        '1.3.6.1.4.1.2011.6.139.13.3.3.1.6',
+        '1.3.6.1.4.1.2011.6.139.13.3.3.1.7',
+        '1.3.6.1.4.1.2011.6.139.13.3.3.1.13',
+        '1.3.6.1.4.1.2011.6.139.13.3.3.1.18'
     ]
-    # oids = [
-    #     '.1.3.6.1.4.1.2011.6.139.13.3.3.1.1',
-    #     '.1.3.6.1.4.1.2011.6.139.13.3.3.1.2',
-    #     '.1.3.6.1.4.1.2011.6.139.13.3.3.1.3',
-    #     '1.3.6.1.4.1.2011.6.139.13.3.3.1.4',
-    #     '1.3.6.1.4.1.2011.6.139.13.3.3.1.5',
-    #     '1.3.6.1.4.1.2011.6.139.13.3.3.1.6',
-    #     '1.3.6.1.4.1.2011.6.139.13.3.3.1.7',
-    #     '1.3.6.1.4.1.2011.6.139.13.3.3.1.13',
-    #     '1.3.6.1.4.1.2011.6.139.13.3.3.1.18'
-    # ]
 
     results = []
 
-    def get_next_oid_value(oid):
+    while True:
         errorIndication, errorStatus, errorIndex, varBinds = next(
             getCmd(SnmpEngine(),
                    UsmUserData(user, authKey, privKey,
@@ -42,27 +37,38 @@ def get_snmp_data():
                                privProtocol=privProtocol),
                    UdpTransportTarget((ip, port)),
                    ContextData(),
-                   ObjectType(ObjectIdentity(oid)),
+                   ObjectType(ObjectIdentity(base_oid)),
                    lookupMib=False)
         )
 
         if errorIndication:
-            print(errorIndication)
+            logging.error(errorIndication)
+            break
         elif errorStatus:
-            print('%s at %s' % (errorStatus.prettyPrint(), errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
+            logging.error('%s at %s', errorStatus.prettyPrint(), errorIndex and varBinds[int(errorIndex) - 1][0] or '?')
+            break
         else:
             for varBind in varBinds:
                 oid = varBind[0].prettyPrint()
                 value = varBind[1].prettyPrint()
-                result = [oid, value]
-                results.append(result)
-                print('OID: %s, Value: %s' % (oid, value))
+                # Check if the returned OID is beyond the base OID
+                if not oid.startswith(base_oid):
+                    break
+                results.append([oid, value])
+                logging.debug('OID: %s, Value: %s', oid, value)
+                # Update the base_oid to the newly returned OID for the next iteration
+                base_oid = oid
+            else:
+                # Continue the loop if no break occurred in the for loop
+                continue
+            # A break occurred in the for loop, so we break the while loop
+            break
 
-    # 保存结果到CSV文件
+        # 保存结果到CSV文件
     with open('snmp_results.csv', 'w', newline='') as csvfile:
         csvwriter = csv.writer(csvfile)
         csvwriter.writerow(['OID', 'Value'])
         csvwriter.writerows(results)
 
-    print("完成，结果已保存到snmp_results.csv")
+    logging.info("完成，结果已保存到snmp_results.csv")
 
