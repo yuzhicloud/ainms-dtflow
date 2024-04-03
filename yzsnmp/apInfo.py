8534dc67526da322e1087eb0643017f38553ddc9
billyhotjava <billy.xiezhimin@gmail.com>
2024-04-03 16:41:09 +0800

    update

--- yzsnmp/apInfo.py
+++ yzsnmp/apInfo.py
@@ -13,42 +13,45 @@ def get_snmp_data():
     authProtocol = usmHMAC192SHA256AuthProtocol
     privProtocol = usmAesCfb256Protocol
 
-    oids = [
-        '1.3.6.1.4.1.2011.6.139.13.3.3.1.1',
-        '1.3.6.1.4.1.2011.6.139.13.3.3.1.2',
-        '1.3.6.1.4.1.2011.6.139.13.3.3.1.3',
-        '1.3.6.1.4.1.2011.6.139.13.3.3.1.4',
-        '1.3.6.1.4.1.2011.6.139.13.3.3.1.5',
-        '1.3.6.1.4.1.2011.6.139.13.3.3.1.6',
-        '1.3.6.1.4.1.2011.6.139.13.3.3.1.7',
-        '1.3.6.1.4.1.2011.6.139.13.3.3.1.13',
-        '1.3.6.1.4.1.2011.6.139.13.3.3.1.18'
-    ]
+    # 起始OID
+    oid_start = '1.3.6.1.4.1.2011.6.139.13.3.3.1.1'
+    # 结束OID，用于判断何时停止遍历
+    oid_end = '1.3.6.1.4.1.2011.6.139.13.3.3.1.18'
 
     results = []
 
-    for oid in oids:
-        errorIndication, errorStatus, errorIndex, varBinds = next(
-            nextCmd(SnmpEngine(),
-                   UsmUserData(user, authKey, privKey,
-                               authProtocol=authProtocol,
-                               privProtocol=privProtocol),
-                   UdpTransportTarget((ip, port), timeout=3, retries=5),
-                   ContextData(),
-                   ObjectType(ObjectIdentity(oid)),
-                   lookupMib=False)  # Use snmpget for each OID
-        )
+    for (errorIndication,
+         errorStatus,
+         errorIndex,
+         varBinds) in bulkCmd(SnmpEngine(),
+                              UsmUserData(user, authKey, privKey,
+                                          authProtocol=authProtocol,
+                                          privProtocol=privProtocol),
+                              UdpTransportTarget((ip, port)),
+                              ContextData(),
+                              0, 25,  # 非重复者，最大重复者
+                              ObjectType(ObjectIdentity(oid_start)),
+                              lexicographicMode=False,  # 不超出指定的OID范围
+                              lookupMib=False):
 
         if errorIndication:
             logging.error(errorIndication)
+            break
         elif errorStatus:
-            logging.error('%s at %s', errorStatus.prettyPrint(), errorIndex and varBinds[int(errorIndex) - 1][0] or '?')
+            logging.error('%s at %s', errorStatus.prettyPrint(), errorIndex and varBinds[int(errorIndex)-1][0] or '?')
+            break
         else:
             for varBind in varBinds:
-                oid = varBind[0].prettyPrint()
-                value = varBind[1].prettyPrint()
-                results.append([oid, value])
-                logging.debug('OID: %s, Value: %s', oid, value)
+                oid, value = varBind
+                if not str(oid).startswith(oid_start[:oid_start.rfind('.')]):
+                    # 如果超出了表的范围，则停止
+                    break
+                results.append([oid.prettyPrint(), value.prettyPrint()])
+                logging.debug('OID: %s, Value: %s', oid.prettyPrint(), value.prettyPrint())
+
+        if not str(oid).startswith(oid_start[:oid_start.rfind('.')]):
+            # 确保当遍历到表的末尾时跳出外层循环
+            break
 
     # 保存结果到CSV文件
     with open('snmp_results.csv', 'w', newline='') as csvfile:
