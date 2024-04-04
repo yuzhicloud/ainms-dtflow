ead991ab2e52629c189259352c12125076cb2e52
billyhotjava <billy.xiezhimin@gmail.com>
2024-04-04 21:21:15 +0800

    update

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
     print(f"Generated CSV file: {csv_file_name}")
 
     # 使用这个CSV文件名作为参数，调用csvToDatabase函数进行数据处理
-    csvToDatabase(csv_file_name)
+    # csvToDatabase(csv_file_name)
 
 
 if __name__ == "__main__":
