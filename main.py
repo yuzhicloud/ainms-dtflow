b50d67dc144e46cb8ff96d5ea5db035c157ceffe
billyhotjava <billy.xiezhimin@gmail.com>
2024-04-15 19:09:02 +0800

    update ap

--- main.py
+++ main.py
@@ -61,19 +61,19 @@ def main():
     csv_dir = 'csvfiles'  # Relative path to csvfiles directory
     # Print current working directory and Python sys.path for debugging
     logging.info("Current working directory: %s", os.getcwd())
-    #
-    # clear_directory(csv_dir)
-    # logging.info("CSV directory cleared.")
-    #
-    # # Call the SNMP main function and get the list of processed CSV files
-    # ips = ['10.170.69.101', '10.170.69.104', '10.170.69.107', '10.170.69.110']
-    # snmp_csv_files = yzsnmp.snmp_main(ips)
-    # logging.debug("Processed files:", snmp_csv_files)
 
-    snmp_csv_files = ['snmp_table_data_10_170_69_101.csv',
-                      'snmp_table_data_10_170_69_104.csv',
-                      'snmp_table_data_10_170_69_107.csv',
-                      'snmp_table_data_10_170_69_110.csv']
+    clear_directory(csv_dir)
+    logging.info("CSV directory cleared.")
+
+    # Call the SNMP main function and get the list of processed CSV files
+    ips = ['10.170.69.101', '10.170.69.104', '10.170.69.107', '10.170.69.110']
+    snmp_csv_files = yzsnmp.snmp_main(ips)
+    logging.debug("Processed files:", snmp_csv_files)
+
+    # snmp_csv_files = ['snmp_table_data_10_170_69_101.csv',
+    #                   'snmp_table_data_10_170_69_104.csv',
+    #                   'snmp_table_data_10_170_69_107.csv',
+    #                   'snmp_table_data_10_170_69_110.csv']
 
     # allapg_file_path = process_ap_name_multithreaded(snmp_csv_files, csv_dir)
     # logging.debug("allAPG file path: %s", allapg_file_path)
@@ -89,7 +89,6 @@ def main():
     truncate_table(engine, 'access_point_group')
 
     apgtodb.load_data_to_database(engine, allapg_file_path, apg_table_name)
-    # aptodb.ap_db_operation(engine, csv_dir, allapg_file_path, snmp_csv_files, ap_table_name)
     ap_csv_files = aptodb.create_csv(csv_dir, allapg_file_path, snmp_csv_files)
     aptodb.insert_csv_data_to_db(engine, ap_csv_files, ap_table_name)
 
